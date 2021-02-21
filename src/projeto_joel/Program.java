032a8b96c02b92198cfbf2e2012efacad026ec0c
Everton Pires <epe.masork@hotmail.com>
2021-02-21 15:59:38 -0300

    modifiquei

--- src/projeto_joel/Program.java
+++ src/projeto_joel/Program.java
@@ -3,11 +3,16 @@ package projeto_joel;
 public class Program {
 
 	public static void main(String[] args) {
-		
-		for(int i = 0; i < 50 ; i++) {
+
+		for (int i = 0; i < 5; i++) {
 			System.out.println("joel - teste");
 		}
-
+		Boolean valor = true;
+		if (valor == false) {
+			System.out.println("ola");
+		}else {
+			System.out.println("ola o carai");
+		}
 	}
 
 }
