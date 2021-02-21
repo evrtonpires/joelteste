f84bf84606cb521fd0958d4cbe1f7b5143f1faed
Everton Pires <epe.masork@hotmail.com>
2021-02-21 15:50:21 -0300

    mudei o nome para repetir 50x joel - teste

--- src/projeto_joel/Program.java
+++ src/projeto_joel/Program.java
@@ -4,7 +4,9 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("joel");
+		for(int i = 0; i < 50 ; i++) {
+			System.out.println("joel - teste");
+		}
 
 	}
 
